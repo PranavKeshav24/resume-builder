340a2f6dbec19209c204c764d2cf320665e79f27
Pranav Keshav <1rn22is047.pranavkeshav@gmail.com>
2025-11-06 08:47:03 +0530

    cors fixes #2

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ import userRouter from "./routes/userRoutes.js";
 import resumeRouter from "./routes/resumeRoutes.js";
 import aiRouter from "./routes/aiRoutes.js";
 import cors from "cors";
-import corsMiddleware from "./middlewares/corsMiddleware.js";
 
 const app = express();
 const PORT = process.env.Port || 3000;
@@ -15,8 +14,27 @@ await connectDB();
 
 dotenv.config();
 
+const allowedOrigins = (
+  process.env.ALLOWED_ORIGINS || "http://localhost:5173"
+).split(",");
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  credentials: true,
+};
+
 app.use(express.json());
-app.use(corsMiddleware);
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => res.send("Server is live..."));
 app.use("/api/users", userRouter);
